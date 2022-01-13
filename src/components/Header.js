491bc2854832736a0bb426ea41c7753799d2456f
charlesgobina <charles.gobina@gmail.com>
2022-01-13 12:34:36 +0100

    Add burger button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import { BiMenu, BiX } from 'react-icons/bi';
 
-const Header = () => (
-  <>
-    <nav className="naviBar">
-      <h1 className="naviTitle">Math Magician</h1>
-      <ul className="naviLinks">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="calculator">Calculator</Link>
-        </li>
-        <li>
-          <Link to="quote">Quote</Link>
-        </li>
-      </ul>
-    </nav>
-  </>
+const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
 
-);
+  const handleToggle = () => {
+    setNavOpen((prev) => !prev);
+  };
+
+  const closeNav = () => {
+    setNavOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <nav className="naviBar">
+        <h1 className="naviTitle">Math Magician</h1>
+        <button onClick={handleToggle} type="button">
+          { navOpen ? (
+            <BiX style={{
+              fontSize: '30px',
+              marginTop: '10px',
+              color: '#f71501',
+            }}
+            />
+          ) : (
+            <BiMenu style={{
+              fontSize: '30px',
+              marginTop: '10px',
+            }}
+            />
+          ) }
+        </button>
+        <ul className="newLinks">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="quote">Quote</Link>
+          </li>
+        </ul>
+        <ul className={`naviLinks menuNav ${navOpen ? ' showMenu' : ''}`}>
+          <li>
+            <Link to="/" onClick={closeNav}>Home</Link>
+          </li>
+          <li>
+            <Link to="calculator" onClick={closeNav}>Calculator</Link>
+          </li>
+          <li>
+            <Link to="quote" onClick={closeNav}>Quote</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
 export default Header;
